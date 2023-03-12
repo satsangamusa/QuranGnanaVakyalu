e0cf9c637d513008793a4770edfec28eada6ac4a
satsangamusa <satsangam.usa@gmail.com>
2023-03-11 22:38:54 -0500

    అచ్చుతప్పులు ఐఓఎస్ మరియు ఆండ్రాయిడ్ కు సరిచేయబడినవి

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.thethreesouls.telugu.qurangnanavakyamulu',
+  appName: 'జ్ఞాన వాక్యాలు',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+export default config;
