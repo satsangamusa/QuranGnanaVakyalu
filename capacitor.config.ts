93a8458fac85964a37fc135b9009b3cbdf2e74ee
satsangamusa <33586258+satsangamusa@users.noreply.github.com>
2024-07-29 09:49:26 -0400

    text corrections, zoom feature added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.thethreesouls.telugu.qurangnanavakyamulu',
   appName: 'జ్ఞాన వాక్యాలు',
-  webDir: 'www/browser'
+  webDir: 'www/browser',
+  zoomEnabled:true
 };
 
 export default config;
